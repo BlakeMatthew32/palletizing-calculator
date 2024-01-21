49b5d619128690011953c82cc1c9d001a257c5fd
Blake Matthew <79722560+BlakeMatthew32@users.noreply.github.com>
2024-01-21 22:17:48 +0000

    Basic styles for Header section

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -2,6 +2,7 @@ const Header = () => {
     return (
         <header className="header">
             <h1 className="header__logo">Robot palletizing Calculator</h1>
+            <button className="header__menu-button">Menu</button>
         </header>
     )
 }
