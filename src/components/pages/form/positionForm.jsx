566a759ef75b891b5aa52569f50c78e2bd5cbabc
Blake Matthew <79722560+BlakeMatthew32@users.noreply.github.com>
2024-02-13 21:36:32 +0000

    Feature: begin working through form logic

--- src/components/pages/form/positionForm.jsx
+++ src/components/pages/form/positionForm.jsx
@@ -1,32 +1,35 @@
 /* eslint-disable react/prop-types */
 
 
-const PositionForm = ({ posNo }) => {
+const PositionForm = ({position, data, updateForm}) => {
+
+    updateForm()
 
     return <div 
     className="form__field-container" >
-        <label htmlFor={`X-${posNo}`}>X: 
-            <input id={`X-${posNo}`}  value="" />   
+        <p>{position}</p>
+        <label htmlFor={`X-${position}`}>X: 
+            <input id={`X-${position}`} value={data[position].x} onChange={(event) => updateForm(data[position].x = event.target.value, position)}/>   
         </label>
 
-        <label htmlFor={`RX-${posNo}`}>RX: 
-            <input id={`RX-${posNo}`}  value="" />
+        <label htmlFor={`RX-${position}`}>RX: 
+            <input id={`RX-${position}`} />
         </label>
 
-        <label htmlFor={`Y-${posNo}`}>Y: 
-            <input id={`Y-${posNo}`}  value="" />
+        <label htmlFor={`Y-${position}`}>Y: 
+            <input id={`Y-${position}`} />
         </label>
 
-        <label htmlFor={`RY-${posNo}`}>RY: 
-            <input id={`RY-${posNo}`}  value="" />
+        <label htmlFor={`RY-${position}`}>RY: 
+            <input id={`RY-${position}`} />
         </label>
 
-        <label htmlFor={`Z-${posNo}`}>Z: 
-            <input id={`Z-${posNo}`}  value="" />
+        <label htmlFor={`Z-${position}`}>Z: 
+            <input id={`Z-${position}`} />
         </label>
 
-        <label htmlFor={`RZ-${posNo}`}>RZ: 
-            <input id={`RZ-${posNo}`}  value="" />
+        <label htmlFor={`RZ-${position}`}>RZ: 
+            <input id={`RZ-${position}`} />
         </label>
     </div>
 }
--- src/components/pages/home/calculatorInput.jsx
+++ src/components/pages/home/calculatorInput.jsx
@@ -1,31 +1,82 @@
+import { useState } from "react"
 import useMultiStepForm from "../../hooks/useMultStepForm"
-// import PositionForm from "../form/positionForm"
+import PositionForm from "../form/positionForm"
+
+const INITITIAL_DATA = {
+    positionOne: {
+        xPos: 0,
+        yPos: 0,
+        zPos: 0,
+        xRotation: 0,
+        yRotation: 0,
+        zRotation: 0
+    }, 
+    positionTwo: {
+        xPos: 0,
+        yPos: 0,
+        zPos: 0,
+        xRotation: 0,
+        yRotation: 0,
+        zRotation: 0
+    }, 
+    positionThree: {
+        xPos: 0,
+        yPos: 0,
+        zPos: 0,
+        xRotation: 0,
+        yRotation: 0,
+        zRotation: 0
+    },
+    positionFour: {
+        xPos: 0,
+        yPos: 0,
+        zPos: 0,
+        xRotation: 0,
+        yRotation: 0,
+        zRotation: 0
+    }, 
+    GridDimentions: {
+        columns: 0,
+        rows: 0
+    }
+}
 
 const CalculatorInput = () => {
+
+    const [formData, setFormData] = useState(INITITIAL_DATA)
+    const updateForm = (currentForm, position) => {
+        setFormData((prev) => {
+            return {
+                ...prev,
+                [position]: {...currentForm}
+            }
+        })
+    }
+
     const {step, stepIndex, back, next, isFirstStep, isLastStep} = useMultiStepForm([
-        <h1 key={1}>step 1</h1>, 
-        <h1 key={2}>step 2</h1>, 
-        <h1 key={3}>step 3</h1>
+        <PositionForm position={'positionOne'} key={1} data={formData} updateForm={updateForm} />,
+        <PositionForm position={'positionTwo'} key={2} data={formData} updateForm={updateForm} />,
+        <PositionForm position={'positionThree'} key={3} data={formData} updateForm={updateForm} />,
+        <PositionForm position={'positionFour'} key={4} data={formData} updateForm={updateForm} />,
     ])
 
-    const handleBack = (event) => {
-        event.preventDefault()
+    const handleBack = () => {
         if(!isFirstStep) back()
     }
 
-    const handleNext = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault()
         if(!isLastStep) next()
     }
 
     return (
         <>
-            <form className="form">
+            <form onSubmit={handleSubmit} className="form">
             <h3>Enter data for Corner {stepIndex + 1}: </h3>
                 {step}
                 <div className="form__button-wrapper">
                     {!isFirstStep && <button onClick={handleBack} type="button">Back</button>}
-                    <button onClick={handleNext} type="button">Next</button>
+                    <button type="submit">Next</button>
                 </div>
             </form>
         </>
